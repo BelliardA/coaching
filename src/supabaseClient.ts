831372d5d1da75c3e08b5450a6a45257d295ade7
arnaud <arn.belliard@gmail.com>
2025-01-04 18:16:33 +0100

    build

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Remplacez par vos clés API et URL de Supabase
-const supabaseUrl = import.meta.env.supabaseUrl;
-const supabaseKey = import.meta.env.supabaseKey;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
